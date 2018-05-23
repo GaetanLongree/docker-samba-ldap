124f8e147041c4853ef8c1807ac528939e8cbfc1
Gaetan Longree <gaetanlongree@gmail.com>
2018-05-23 15:14:41 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM debian:latest
+LABEL maintainer="gaetanlongree@gmail.com"
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+ENV LDAP_CAPABLE 0
+ENV WORKGROUP WORKGROUP
+ENV LDAP_HOST 127.0.0.1
+ENV LDAP_SUFIX dc=contoso,dc=com
+ENV LDAP_USERS_SUFIX ou=Users,dc=contoso,dc=com
+ENV LDAP_GROUPS_SUFIX ou=Groups,dc=contoso,dc=com
+ENV LDAP_MACHINES_SUFIX ou=Computers,dc=contoso,dc=com
+ENV LDAP_ADMIN_DN cn=admin,dc=contoso,dc=com
+ENV LDAP_ADMIN_PASSWD P@$$w0rd
+ENV LDAP_SSL no
+ENV LDAP_TLS 0
+ENV LDAP_BASE_DN ou=Users,dc=contoso,dc=com
+
+RUN apt-get update && \
+	apt-get install --no-install-recommends --no-install-suggests -y \
+	samba libnss-ldap smbldap-tools ldap-utils wget perl make &&\
+	cp -R /etc/samba /tmp/
+
+COPY entrypoint.sh /
+
+VOLUME /etc
+VOLUME /var/lib/samba/private
+VOLUME /share
+
+EXPOSE 139
+EXPOSE 445
+
+ENTRYPOINT ["bash", "/entrypoint.sh"]
+
+CMD ["/usr/sbin/smbd", "-F", "-S", "-D"]
---
+++ entrypoint.sh
@@ -0,0 +1,118 @@
+#!/bin/bash 
+
+if [ $LDAP_CAPABLE = 1 ]; then
+	pushd /
+	wget https://gist.githubusercontent.com/Spredzy/6389499/raw/e503ecc6bb530316b8d1db9c1f33d89a677e66d3/Crypt-SmbHash --no-check-certificate -O script
+	pushd /tmp
+        wget http://search.cpan.org/CPAN/authors/id/B/BJ/BJKUIT/Crypt-SmbHash-0.12.tar.gz
+        tar xzf Crypt-SmbHash-0.12.tar.gz
+	pushd /tmp/Crypt-SmbHash-0.12
+        perl Makefile.PL
+        make && make test && make install
+fi
+
+if [ ! -d /etc/samba ]; then
+	mkdir /etc/samba
+fi
+
+if [ ! -d /etc/smbldap-tools ]; then
+	mkdir /etc/smbldap-tools
+fi
+
+if [ $LDAP_CAPABLE = 1 ]; then
+if [ ! -f /etc/samba/smb.conf ]; then
+cat > /etc/samba/smb.conf << EOF
+[global]
+   workgroup = $WORKGROUP
+   dns proxy = no
+
+   passdb backend = ldapsam:ldap://$LDAP_HOST/
+   ldap suffix = $LDAP_SUFIX
+   ldap user suffix = $LDAP_USERS_SUFIX
+   ldap group suffix = $LDAP_GROUPS_SUFIX
+   ldap machine suffix = $LDAP_MACHINES_SUFIX
+   ldap admin dn = $LDAP_ADMIN_DN
+   ldap ssl = $LDAP_SSL
+   ldap passwd sync = yes
+
+   security = user
+
+[share]
+    comment = Some share
+    path = /share
+    browseable = yes
+    read only = no
+    valid users = usera userb
+EOF
+fi
+
+if [ ! -f /etc/nslcd.conf ]; then
+cat > /etc/nslcd.conf << EOF
+uid nslcd
+gid nslcd
+uri ldap://$LDAP_HOST/
+base $LDAP_BASE_DN
+binddn $LDAP_ADMIN_DN
+bindpw $LDAP_ADMIN_PASSWD
+EOF
+fi
+
+if [ ! -f /etc/smbldap-tools/smbldap.conf ]; then
+cat > /etc/smbldap-tools/smbldap.conf << EOF
+sambaDomain="$WORKGROUP"
+slaveLDAP="$LDAP_HOST"
+slavePort="389"
+masterLDAP="$LDAP_HOST"
+masterPort="389"
+ldapTLS="$LDAP_TLS"
+verify=""
+cafile=""
+clientcert=""
+clientkey=""
+suffix="$LDAP_SUFIX"
+usersdn="$LDAP_USERS_SUFIX"
+computersdn="$LDAP_MACHINES_SUFIX"
+groupsdn="$LDAP_GROUPS_SUFIX"
+idmapdn="$LDAP_USERS_SUFIX"
+sambaUnixIdPooldn="sambaDomainName=$(hostname),${suffix}"
+scope="sub"
+hash_encrypt="SSHA"
+crypt_salt_format=""
+userLoginShell="/bin/bash"
+userHome="/home/%U"
+userHomeDirectoryMode="700"
+userGecos="System User"
+defaultUserGid="513"
+defaultComputerGid="515"
+skeletonDir="/etc/skel"
+defaultMaxPasswordAge="45"
+userSmbHome="\\\%U"
+userProfile="\\\profiles\%U"
+userHomeDrive=""
+userScript=""
+mailDomain="iglu.lu"
+with_smbpasswd="0"
+smbpasswd="/usr/bin/smbpasswd"
+with_slappasswd="0"
+slappasswd="/usr/sbin/slappasswd"
+EOF
+fi
+
+if [ ! -f /etc/smbldap-tools/smbldap_bind.conf ]; then
+cat > /etc/smbldap-tools/smbldap_bind.conf << EOF
+slaveDN="$LDAP_ADMIN_DN"
+slavePw="$LDAP_ADMIN_PASSWD"
+masterDN="$LDAP_ADMIN_DN"
+masterPw="$LDAP_ADMIN_PASSWD"
+EOF
+fi
+
+smbpasswd -w $LDAP_ADMIN_PASSWD
+fi
+
+# If LDAP is not enabled, copy original files from tmp to their original location
+if [ -z "$(ls -A /etc/samba)" ]; then
+	cp -R /tmp/samba/* /etc/samba
+fi
+
+exec "$@"
